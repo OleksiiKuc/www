10ff07a92993a4882cdfbf0241a17e235d38e894
OleksiiKuc <74044099+OleksiiKuc@users.noreply.github.com>
2021-05-13 20:03:38 +0300

    without one section and adaptive

---
+++ Cuba/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.photo__slider').slick({
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/Arrow-left.svg" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/Arrow-right.svg" alt="next"></button>'
+      });
+
+
+});
