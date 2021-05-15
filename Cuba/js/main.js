f6e3af1dc5b181941aaced90d332b65d279ba770
OleksiiKuc <74044099+OleksiiKuc@users.noreply.github.com>
2021-05-15 18:10:42 +0300

    add burger and fixed header

--- Cuba/js/main.js
+++ Cuba/js/main.js
@@ -8,5 +8,45 @@ $(function(){
         nextArrow: '<button class="slick-btn slick-next"><img src="images/Arrow-right.svg" alt="next"></button>'
       });
 
+    let nav = $('#menu');
+    let header = $('#header');
+    let headerH = header.height();
+    let navH = nav.height();
+    let scrollPos = $(window).scrollTop();
 
+    $(window).scroll(function() {
+      scrollPos = $(this).scrollTop();
+      headerH = header.height();
+      if(scrollPos > headerH) {
+        nav.addClass('header_fixed');
+        $('body').css({
+          'paddingTop' : navH + 'px'  
+        });
+      } else {
+        nav.removeClass('header_fixed');
+        $('body').css({
+          'paddingTop' : 0
+        });
+      }
+      if(scrollPos > headerH) {
+        navToggle.addClass('header_fixed');
+        $('body').css({
+          'paddingTop' : navH + 'px'  
+        });
+      } else {
+        navToggle.removeClass('header_fixed');
+        $('body').css({
+          'paddingTop' : 0
+        });
+      }
+    });
+
+    let navToggle = $("#navToggle");
+
+    /* Nav Toggle */
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
 });
